dc2f85ab63dfb559e314ac46791334b0cfaaef78
Michael Brown <mike_brown@rogers.com>
2017-02-02 19:06:41 -0500

    Camera server and handling

--- src/org/usfirst/frc/team4308/robot/Robot.java
+++ src/org/usfirst/frc/team4308/robot/Robot.java
@@ -1,6 +1,10 @@
 
 package org.usfirst.frc.team4308.robot;
 
+import edu.wpi.cscore.AxisCamera;
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -17,6 +21,10 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc.team4308.robot.commands.ExampleCommand;
 import org.usfirst.frc.team4308.robot.subsystems.ExampleSubsystem;
 
@@ -36,6 +44,7 @@ public class Robot extends IterativeRobot implements PIDOutput {
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
 
+	Thread visionThread;
 	RobotDrive robot;
 	Joystick stick;
 	CANTalon shooter;
@@ -68,7 +77,7 @@ public class Robot extends IterativeRobot implements PIDOutput {
 		} catch (RuntimeException rte) {
 			DriverStation.reportError("Error instantiating navX-MXP: " + rte.getStackTrace(), true);
 		}
-		
+
 		turnController = new PIDController(kP, kI, kD, kF, gyro, this);
 		turnController.setInputRange(-180.0f, 180.0f);
 		turnController.setOutputRange(-1.0, 1.0);
@@ -81,6 +90,42 @@ public class Robot extends IterativeRobot implements PIDOutput {
 
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
+		
+		visionThread = new Thread(() -> {
+			// Get the Axis camera from CameraServer
+			AxisCamera camera = CameraServer.getInstance().addAxisCamera("axis-camera.local");
+			// Set the resolution
+			camera.setResolution(640, 480);
+
+			// Get a CvSink. This will capture Mats from the camera
+			CvSink cvSink = CameraServer.getInstance().getVideo();
+			// Setup a CvSource. This will send images back to the Dashboard
+			CvSource outputStream = CameraServer.getInstance().putVideo("Rectangle", 640, 480);
+
+			// Mats are very memory expensive. Lets reuse this Mat.
+			Mat mat = new Mat();
+
+			// This cannot be 'true'. The program will never exit if it is. This
+			// lets the robot stop this thread when restarting robot code or
+			// deploying.
+			while (!Thread.interrupted()) {
+				// Tell the CvSink to grab a frame from the camera and put it
+				// in the source mat.  If there is an error notify the output.
+				if (cvSink.grabFrame(mat) == 0) {
+					// Send the output the error.
+					outputStream.notifyError(cvSink.getError());
+					// skip the rest of the current iteration
+					continue;
+				}
+				// Put a rectangle on the image
+				Imgproc.rectangle(mat, new Point(100, 100), new Point(400, 400),
+						new Scalar(255, 255, 255), 5);
+				// Give the output stream a new image to display
+				outputStream.putFrame(mat);
+			}
+		});
+		visionThread.setDaemon(true);
+		visionThread.start();
 	}
 
 	/**
@@ -156,6 +201,7 @@ public class Robot extends IterativeRobot implements PIDOutput {
 			shooter.set(0);
 		}
 		SmartDashboard.putNumber("Speed: ", shooter.getAnalogInPosition());
+		SmartDashboard.putNumber("NavX: ", gyro.getAngle());
 	}
 
 	/**
