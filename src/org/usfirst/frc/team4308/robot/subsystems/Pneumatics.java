5af1b5a1673d7a4e39a29b40413d8810f1d333eb
Michael Brown <mike_brown@rogers.com>
2017-02-21 22:19:45 -0500

    pneumatics adaptation

--- src/org/usfirst/frc/team4308/robot/subsystems/Pneumatics.java
+++ src/org/usfirst/frc/team4308/robot/subsystems/Pneumatics.java
@@ -1,28 +1,19 @@
 package org.usfirst.frc.team4308.robot.subsystems;
 
-import org.usfirst.frc.team4308.robot.RobotMap;
 import org.usfirst.frc.team4308.robot.commands.WaitForPressure;
 import org.usfirst.frc.team4308.util.Loggable;
 
-import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Pneumatics extends Subsystem implements Loggable { // TODO: adapt to actual pneumatics application
+public class Pneumatics extends Subsystem implements Loggable {
 
-	private static final double maxPressure = 2.55;
-
-	private AnalogInput pressureSensor;
 	private Compressor compressor;
 
 	public Pneumatics() {
 		super();
 		compressor = new Compressor();
-		pressureSensor = new AnalogInput(RobotMap.pressureSensorChannel);
-
-		LiveWindow.addSensor("Pneumatics", "Pressure Sensor", pressureSensor);
 	}
 
 	@Override
@@ -31,7 +22,8 @@ public class Pneumatics extends Subsystem implements Loggable { // TODO: adapt t
 	}
 
 	/**
-	 * Start the compressor going. The compressor automatically starts and stops as it goes above and below maximum pressure.
+	 * Start the compressor going. The compressor automatically starts and stops
+	 * as it goes above and below maximum pressure.
 	 */
 	public void start() {
 		compressor.start();
@@ -41,12 +33,13 @@ public class Pneumatics extends Subsystem implements Loggable { // TODO: adapt t
 	 * @return Whether or not the system is fully pressurized.
 	 */
 	public boolean isPressurized() {
-		return maxPressure <= pressureSensor.getVoltage();
+		return compressor.getPressureSwitchValue();
 	}
 
 	@Override
 	public void log() {
-		SmartDashboard.putNumber("Pressure", pressureSensor.getVoltage());
+		SmartDashboard.putNumber("Pressure", compressor.getCompressorCurrent());
+		SmartDashboard.putBoolean("Pressurized", isPressurized());
 	}
 
 }
